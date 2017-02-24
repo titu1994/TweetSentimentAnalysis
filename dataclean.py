5d6b73941ea5673645fc900c9b504a95d0bbbee3
Somshubra Majumdar <titu1994@gmail.com>
2017-02-23 21:03:18 -0600

    Cleaned the data (removed http links, <> and trimed)

--- dataclean.py
+++ dataclean.py
@@ -1,7 +1,24 @@
 import pandas as pd
 import numpy as np
+import sys
 
-obama_df = pd.read_csv('data/obama_csv.csv', encoding='latin1', sep='|')
-romney_df = pd.read_csv('data/romney_csv.csv', encoding='latin1', sep='|')
+data_path = r"data/training-Obama-Romney-tweets.xlsx"
+obama_df = pd.read_excel(data_path, sheetname=0)
+romney_df = pd.read_excel(data_path, sheetname=1)
 
-print(obama_df.head())
+# obama_df.to_csv('data/obama_csv.csv', sep='|', index_label='id')
+# romney_df.to_csv('data/romney_csv.csv', sep='|', index_label='id')
+#
+# obama_df = pd.read_csv('data/obama_csv.csv', encoding='latin1', sep='|')
+# romney_df = pd.read_csv('data/romney_csv.csv', encoding='latin1', sep='|')
+
+def clean_text(row):
+    row = str(row).encode('ascii', 'ignore').strip()
+    return row
+
+obama_df['tweet'] = obama_df['tweet'].apply(clean_text)
+romney_df['tweet'] = romney_df['tweet'].apply(clean_text)
+
+print("Writing cleaned op")
+obama_df.to_csv('data/obama_csv.csv', sep='\t')
+romney_df.to_csv('data/romney_csv.csv', sep='\t')
