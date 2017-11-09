450a0cf8dc553ab99a2e8533289f665fda36c5e1
Somshubra Majumdar <titu1994@gmail.com>
2017-11-09 12:48:41 -0600

    Update documentation

--- keras_utils.py
+++ keras_utils.py
@@ -1,15 +1,12 @@
-import pandas as pd
 import numpy as np
 import os
 import pickle
 import glob
 import ast
-import re
 
 np.random.seed(1000)
 
-from sklearn_utils import load_both, load_obama, load_romney, classification_report, f1_score, accuracy_score, \
-    confusion_matrix
+from sklearn_utils import load_both, load_obama, load_romney
 from sklearn.model_selection import StratifiedKFold
 
 from keras.preprocessing.text import Tokenizer
@@ -28,6 +25,7 @@ train_romney_full_path = "data/full_romney_csv.csv"
 test_obama_path = "data/obama_csv_test.csv"
 test_romney_path = "data/romney_csv_test.csv"
 
+# list of all models and their corresponding directories
 model_dirs = ['conv/', 'n_conv/', 'lstm/', 'bidirectional_lstm/', 'multiplicative_lstm/']
 
 
@@ -68,6 +66,22 @@ def fbeta_score(y_true, y_pred):
 
 
 def load_embedding_matrix(embedding_path, word_index, max_nb_words, embedding_dim, print_error_words=True):
+    '''
+    Either loads the created embedding matrix at run time, or uses the
+    GLoVe 840B word embedding to create a mini initialized embedding matrix
+    for use by Keras Embedding layers
+
+    Args:
+        embedding_path: path to the 840B word GLoVe Embeddings
+        word_index: indices of all the words in the current corpus
+        max_nb_words: maximum number of words in corpus
+        embedding_dim: the size of the embedding dimension
+        print_error_words: Optional, allows to print words from GLoVe
+            that could not be parsed correctly.
+
+    Returns:
+        An Embedding matrix in numpy format
+    '''
     if not os.path.exists('data/embedding_matrix max words %d embedding dim %d.npy' % (max_nb_words, embedding_dim)):
         embeddings_index = {}
         error_words = []
@@ -75,6 +89,7 @@ def load_embedding_matrix(embedding_path, word_index, max_nb_words, embedding_di
         print("Creating embedding matrix")
         print("Loading : ", embedding_path)
 
+        # read the entire GLoVe embedding matrix
         f = open(embedding_path, encoding='utf8')
         for line in f:
             values = line.split()
@@ -87,6 +102,7 @@ def load_embedding_matrix(embedding_path, word_index, max_nb_words, embedding_di
 
         f.close()
 
+        # check for words that could not be loaded properly
         if len(error_words) > 0:
             print("%d words could not be added." % (len(error_words)))
             if print_error_words:
@@ -105,6 +121,7 @@ def load_embedding_matrix(embedding_path, word_index, max_nb_words, embedding_di
                 # words not found in embedding index will be all-zeros.
                 embedding_matrix[i] = embedding_vector
 
+        # save the constructed embedding matrix in a file for efficient loading next time
         np.save('data/embedding_matrix max words %d embedding dim %d.npy' % (max_nb_words,
                                                                              embedding_dim),
                 embedding_matrix)
@@ -112,6 +129,7 @@ def load_embedding_matrix(embedding_path, word_index, max_nb_words, embedding_di
         print('Saved embedding matrix')
 
     else:
+        # load pre-built embedding matrix
         embedding_matrix = np.load('data/embedding_matrix max words %d embedding dim %d.npy' % (max_nb_words,
                                                                                                 embedding_dim))
 
@@ -121,6 +139,7 @@ def load_embedding_matrix(embedding_path, word_index, max_nb_words, embedding_di
 
 
 def create_ngram_set(input_list, ngram_value=2):
+    # construct n-gram text from uni-gram text input
     return set(zip(*[input_list[i:] for i in range(ngram_value)]))
 
 
@@ -139,26 +158,39 @@ def add_ngram(sequences, token_indice, ngram_range=2):
 
 
 def prepare_tokenized_data(texts, max_nb_words, max_sequence_length, ngram_range=2):
-    if not os.path.exists('data/tokenizer.pkl'):
-        tokenizer = Tokenizer(num_words=max_nb_words)
-        tokenizer.fit_on_texts(texts)
+    '''
+    Tokenize the data from sentences to list of words
+
+    Args:
+        texts: sentences list
+        max_nb_words: maximum vocabulary size in text corpus
+        max_sequence_length: maximum length of sentence
+        ngram_range: n-gram of sentences
+
+    Returns:
+        A list of tokenized sentences and the word index list which
+        maps words to an integer index.
+    '''
+    if not os.path.exists('data/tokenizer.pkl'): # check if a prepared tokenizer is available
+        tokenizer = Tokenizer(num_words=max_nb_words)  # if not, create a new Tokenizer
+        tokenizer.fit_on_texts(texts)  # prepare the word index map
 
         with open('data/tokenizer.pkl', 'wb') as f:
-            pickle.dump(tokenizer, f)
+            pickle.dump(tokenizer, f)  # save the prepared tokenizer for fast access next time
 
         print('Saved tokenizer.pkl')
     else:
-        with open('data/tokenizer.pkl', 'rb') as f:
+        with open('data/tokenizer.pkl', 'rb') as f:  # simply load the prepared tokenizer
             tokenizer = pickle.load(f)
             print('Loaded tokenizer.pkl')
 
-    sequences = tokenizer.texts_to_sequences(texts)
-    word_index = tokenizer.word_index
+    sequences = tokenizer.texts_to_sequences(texts)  # transform text into integer indices lists
+    word_index = tokenizer.word_index  # obtain the word index map
     print('Found %s unique 1-gram tokens.' % len(word_index))
 
     ngram_set = set()
     for input_list in sequences:
-        for i in range(2, ngram_range + 1):
+        for i in range(2, ngram_range + 1):  # prepare the n-gram sentences
             set_of_ngram = create_ngram_set(input_list, ngram_value=i)
             ngram_set.update(set_of_ngram)
 
@@ -170,95 +202,110 @@ def prepare_tokenized_data(texts, max_nb_words, max_sequence_length, ngram_range
     indice_token = {token_indice[k]: k for k in token_indice}
     word_index.update(token_indice)
 
-    max_features = np.max(list(indice_token.keys())) + 1
+    max_features = np.max(list(indice_token.keys())) + 1  # compute maximum number of n-gram "words"
     print('Now there are:', max_features, 'features')
 
     # Augmenting X_train and X_test with n-grams features
-    sequences = add_ngram(sequences, token_indice, ngram_range)
-    print('Average sequence length: {}'.format(np.mean(list(map(len, sequences)), dtype=int)))
-    print('Max sequence length: {}'.format(np.max(list(map(len, sequences)))))
+    sequences = add_ngram(sequences, token_indice, ngram_range)  # add n-gram features to original dataset
+    print('Average sequence length: {}'.format(np.mean(list(map(len, sequences)), dtype=int)))  # compute average sequence length
+    print('Max sequence length: {}'.format(np.max(list(map(len, sequences)))))  # compute maximum sequence length
 
-    data = pad_sequences(sequences, maxlen=max_sequence_length)
+    data = pad_sequences(sequences, maxlen=max_sequence_length)  # pad the sequence to the user defined max length
 
     return (data, word_index)
 
 
 def train_keras_model_cv(model_gen, model_fn, max_nb_words=16000, max_sequence_length=140,
                          k_folds=3, nb_epoch=40, batch_size=100, seed=1000):
-    data, labels, texts, word_index = prepare_data(max_nb_words, max_sequence_length)
+    '''
+    Trains a provided Keras model with Stratified Cross Validation.
+
+    Args:
+        model_gen: a function which returns a Keras model
+        model_fn: a string file name for the model to serialize the weights
+        max_nb_words: maximum number of words in embedding
+        max_sequence_length: maximum user defined sequence length
+        k_folds: number of folds to train
+        nb_epoch: number of epochs of training
+        batch_size: batchsize of training each epoch
+        seed: random seed for Stratified KFold. Keras ops are inherently
+            non-deterministic due to use of CUDA and cuDNN to train models.
+    '''
+    data, labels, texts, word_index = prepare_data(max_nb_words, max_sequence_length)  # load the text dataset
 
     print("Dataset :", data.shape)
-    skf = StratifiedKFold(k_folds, shuffle=True, random_state=seed)
+    skf = StratifiedKFold(k_folds, shuffle=True, random_state=seed)  # initialize Stratified Fold Generator
 
     fbeta_scores = []
 
-    for i, (train_idx, test_idx) in enumerate(skf.split(texts, labels)):
-        x_train, y_train = data[train_idx, :], labels[train_idx]
-        x_test, y_test = data[test_idx, :], labels[test_idx]
+    for i, (train_idx, test_idx) in enumerate(skf.split(texts, labels)):  # for each fold
+        x_train, y_train = data[train_idx, :], labels[train_idx]  # obtain the Train samples and labels
+        x_test, y_test = data[test_idx, :], labels[test_idx]  # obtain the Test samples and labels
 
-        y_train_categorical = to_categorical(np.asarray(y_train))
-        y_test_categorical = to_categorical(np.asarray(y_test))
+        y_train_categorical = to_categorical(np.asarray(y_train))  # convert to one-hot representation
+        y_test_categorical = to_categorical(np.asarray(y_test))  # convert to one-hot representation
 
-        K.clear_session()
+        K.clear_session()  # reset GPU memory
 
+        # generate a new Keras model
         model = model_gen()  # type: Model
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc', fbeta_score])
+        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc', fbeta_score])  # compile model
 
+        # save model which obtains best validation f1-score
         model_checkpoint = ModelCheckpoint('models/%s-cv-%d.h5' % (model_fn, i + 1), monitor='val_fbeta_score',
                                            verbose=2,
                                            save_weights_only=True,
                                            save_best_only=True, mode='max')
 
+        # reduce learning rate as schedule demands
         reduce_lr = ReduceLROnPlateau(monitor='val_fbeta_score', patience=5, mode='max',
                                       factor=0.8, cooldown=5, min_lr=1e-6, verbose=2)
 
+        # train model
         model.fit(x_train, y_train_categorical, validation_data=(x_test, y_test_categorical),
-                  callbacks=[model_checkpoint, reduce_lr], nb_epoch=nb_epoch, batch_size=batch_size)
+                  callbacks=[model_checkpoint, reduce_lr], epochs=nb_epoch, batch_size=batch_size)
 
+        # load the saved best weights
         model.load_weights('models/%s-cv-%d.h5' % (model_fn, i + 1))
 
+        # evaluate final performance of the model on test set
         scores = model.evaluate(x_test, y_test_categorical, batch_size=batch_size)
+
+        # save the f1 score for final averaging
         fbeta_scores.append(scores[-1])
 
         print('\nF1 Scores of Cross Validation %d: %0.4f' % (i + 1, scores[-1]))
 
+        # delete Keras model from CPU memory
         del model
 
+    # compute average f1 score over all folds
     print("Average fbeta score : ", sum(fbeta_scores) / len(fbeta_scores))
 
+    # save the f1 score results per fold into a file
     with open('models/%s-scores.txt' % (model_fn), 'w') as f:
         f.write(str(fbeta_scores))
 
 
-def train_full_model(model_gen, model_fn, max_nb_words=16000, max_sequence_length=140, use_full_data=False,
-                     nb_epoch=40, batch_size=100, seed=1000):
-    np.random.seed(seed)
-    data, labels, texts, word_index = prepare_data(max_nb_words, max_sequence_length, use_full_data)
-
-    labels_categorical = to_categorical(np.asarray(labels))
-
-    model = model_gen()
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', fbeta_score])
-
-    model_checkpoint = ModelCheckpoint('models/%s-final.h5' % (model_fn), monitor='val_fbeta_score', verbose=2,
-                                       save_weights_only=True,
-                                       save_best_only=True, mode='max')
-
-    reduce_lr = ReduceLROnPlateau(monitor='val_fbeta_score', patience=5, mode='max',
-                                  factor=0.8, cooldown=5, min_lr=1e-6, verbose=2)
-
-    model.fit(data, labels_categorical, validation_data=(data, labels_categorical),
-              callbacks=[model_checkpoint, reduce_lr], nb_epoch=nb_epoch, batch_size=batch_size)
-
-    print('Finished Training model')
-
-    model.load_weights('models/%s-final.h5' % (model_fn))
-
-    scores = model.evaluate(texts, labels_categorical, batch_size=batch_size)
-    print('\nTraining F1 Scores of Cross Validation: %0.4f' % (scores[-1]))
-
-
 def prepare_data(max_nb_words, max_sequence_length, mode='train', dataset='full'):
+    '''
+    Loads the appropriate dataset as required
+
+    Args:
+        max_nb_words: maximum vocabulary size
+        max_sequence_length: maximum length of a sentence
+        mode: decided which dataset to load. Can be one of
+            'train' or 'test'.
+        dataset: decides which dataset to load.
+            Can be one of :
+            -   'full' (for Joint Training)
+            -   'obama' (for just Obama dataset)
+            -   'romney' (for just Romney dataset)
+
+
+    Returns:
+        The preprocessed text data, labels, the raw text sentences and the word indices
+    '''
     assert dataset in ['full', 'obama', 'romney']
 
     print('Loading %s data' % mode)
@@ -278,6 +325,17 @@ def prepare_data(max_nb_words, max_sequence_length, mode='train', dataset='full'
 
 
 def get_keras_scores(normalize_scores=False):
+    '''
+    Utility function for computing the scores of all the
+    the Keras models from the serialized score lists.
+
+    Args:
+        normalize_scores: whether to normalize the scores
+            Normalization is done by weighing of the sum of weights
+
+    Returns:
+        a list of classifier scores for all Keras models
+    '''
     clf_scores = []
 
     for m, model_dir in enumerate(model_dirs):
@@ -297,62 +355,6 @@ def get_keras_scores(normalize_scores=False):
         clf_scores = weights
 
     return clf_scores
-
-
-def get_predictions_keras_models(models, data, save_dir, normalize_weights=False):
-    model_preds = []
-    clf_scores = []
-
-    assert len(models) == len(model_dirs), 'Number of provided models must match ' \
-                                           'number of model directories specified in keras_utils'
-
-    for m, model_dir in enumerate(model_dirs):
-        path = 'models/' + model_dir + '*.h5'
-        weights_path = 'models/' + model_dir + '*.txt'
-
-        weight_path = glob.glob(weights_path)
-        print('Loading weight file [0]:', weight_path)
-
-        with open(weight_path[0], 'r') as f:
-            clf_weight_data = ast.literal_eval(f.readline())
-
-        fns = glob.glob(path)
-        cv_ids = []
-        for i in range(len(fns)):
-            fn = fns[i]
-            cv_id = re.search(r'\d+', fn).group()
-            cv_ids.append(int(cv_id))
-
-        clf_weight_data = [clf_weight_data[i - 1] for i in cv_ids]
-        clf_scores.extend(clf_weight_data)
-
-        model = models[m]  # type: Model
-
-        temp_preds = np.zeros((len(cv_ids), data.shape[0], 3))
-
-        for j, fn in enumerate(fns):
-            model.load_weights(fn)
-            preds = model.predict(data, batch_size=100)
-            temp_preds[j, :, :] = preds
-
-            print('Got predictions for model - %s' % (fn))
-
-        model_preds.append(temp_preds)  # temp_preds.mean(axis=0)
-
-        preds_save_path = save_dir + "/" + model_dir + os.path.splitext(os.path.basename(weight_path[0]))[0] + '.npy'
-        preds = temp_preds.mean(axis=0)
-
-        np.save(preds_save_path, preds)
-        print('Saved predictions for %s in %s' % (model_dir[:-1], preds_save_path))
-
-        print()
-
-    if normalize_weights:
-        weight_sum = np.sum(np.asarray(clf_scores, dtype=np.float32))
-        weights = [w / weight_sum for w in clf_scores]
-        clf_scores = weights
-
-    return (model_preds, clf_scores)
 
 
 if __name__ == '__main__':
